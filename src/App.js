88d14ec55ada9b017b375504990ad30b18f0ba54
sler1414 <sler1414@gmail.com>
2022-05-30 12:49:13 +0800

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import profileImage from './img/1148429.jpg';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My reacrt app</h1>
+      <h2>Author: Christler</h2>
+      <img src= {profileImage} alt='profile-image'/>
+    </div>
+  );
+}
+
+export default App;
